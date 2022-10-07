f7e913f15d64f6afc248aacfce7d9ee7bc62636b
aryankuag <alemantrixaryan@gmail.com>
2022-10-07 05:34:26 +0530

    looks cool

--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import DrumPad from "./DrumPad";
 
 function DrumMachine() {
+  const [display, setDisplay] = useState(
+    "DISPLAY"
+  )
+
   return (
     <div
       id="drum-machine"
-      className="flex bg-gray-300 items-center gap-8 px-8 border-4 border-gray-100"
+      className="flex bg-[#222831] items-center gap-8 px-8 border-4 border-[#393E46]"
     >
       <div className="grid grid-cols-3 grid-rows-3 gap-2 w-96 h-96 p-5 font-bold">
         {Array.from(["Q", "W", "E", "A", "S", "D", "Z", "X", "C"], (x) => (
-          <DrumPad innerText={x} />
+          <DrumPad innerText={x} event={setDisplay}/>
         ))}
       </div>
-      <div id="display" className="w-40 bg-gray-400 rounded-md p-2">
-        DISPLAY
+      <div id="display" className="w-40 bg-gray-400 font-extrabold rounded-md p-2 text-center text-[#222831]">
+        {display}
       </div>
     </div>
   );
--- src/DrumPad.js
+++ src/DrumPad.js
@@ -1,9 +1,22 @@
 import React from "react";
 
-function DrumPad({ innerText }) {
+function DrumPad({ innerText ,event}) {
+  const playAudio = audio => {
+    event(innerText)
+    const audioToPlay = new Audio(`audio/${innerText}.wav`);
+    audioToPlay.play();
+  }
+
+
   return (
-    <div className="drum-pad w-full h-full bg-red-400 flex justify-center items-center text-2xl rounded-md">
-      <p>{innerText}</p>
+    <div className="drum-pad w-full h-full bg-[#393E46] flex justify-center items-center text-2xl rounded-md cursor-pointer" id={innerText} onClick={playAudio}>
+      <audio>     
+  
+  <source src={`audio/${innerText}.wav`} type="audio/wav" className="clip" id={innerText}/>
+
+
+      </audio>
+      <p className="text-[#fff]">{innerText}</p>
     </div>
   );
 }
